97bcb3d4bbd405aff997eadc603228dc5a4042d7
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-04-19 16:19:48 -0700

    boxes created based on user input

---
+++ script.js
@@ -0,0 +1,13 @@
+function createGrid(num){
+    for (var i = 0; i < num; i++)
+        $('#main').append('<div class="box col-xs-1 col-md-1" id="box'+ i + '"></div>');
+}
+
+$('document').ready(function(){
+    console.log('ready');
+    $('#go').click(function(){
+        var x = $('#size').val();
+        $('#main').find('.box').remove();
+        createGrid(x);
+    });
+});
